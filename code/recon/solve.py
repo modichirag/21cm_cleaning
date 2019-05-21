c20c31be388889b2eeb4bc1e3a1f7d50a56f6d70
modichirag <modichirag@berkeley.edu>
2019-05-21 12:50:20 -0700

    house clean

--- code/recon/solve.py
+++ code/recon/solve.py
@@ -7,19 +7,19 @@ from abopt.algs.lbfgs import scalar as scalar_diag
 import os
 
 #optimizer
-def solve(Nmesh, x0, rtol, run, Nsm, prefix, mock_model, obj, data_p, truth_pm, optfolder, saveit=20, showit=5, title=None, maxiter=100):
+def solve(Nmesh, x0, rtol, run, Nsm, prefix, mock_model, obj, data_p, truth_pm, optfolder, saveit=20, showit=5, title=None, maxiter=100, linesearchiter=20):
     
     pm = truth_pm.resize(Nmesh=(Nmesh, Nmesh, Nmesh))
     atol = pm.Nmesh.prod() * rtol
-    data = data_p
+    #data = data_p
  
-    prior, chi2 = obj.get_code().compute(['prior', 'chi2'], init={'parameters': data.s})
+    prior, chi2 = obj.get_code().compute(['prior', 'chi2'], init={'parameters': data_p.s})
     if pm.comm.rank == 0:
         print('\nPrior and chi2 at data.s \n',  prior, chi2) # for 2d chi2 is close to total pixels.
  
     #Evaluate and save 0 point
-    fit_p = mock_model.make_observable(data.s)
-    r = dg.evaluate(fit_p, data)
+    fit_p = mock_model.make_observable(data_p.s)
+    r = dg.evaluate(fit_p, data_p)
  
     try:
         os.makedirs(optfolder + '%s' % run)
@@ -36,7 +36,7 @@ def solve(Nmesh, x0, rtol, run, Nsm, prefix, mock_model, obj, data_p, truth_pm, 
     dg.save_report(r, optfolder + "%s/truth.png" % run, pm)
     dg.save_2ptreport(r, optfolder + "%s/2pt/truth.png" % run, pm)
  
-    optimizer = LBFGS(m=10, diag_update=scalar_diag, maxiter=maxiter)
+    optimizer = LBFGS(m=10, diag_update=scalar_diag, maxiter=maxiter, linesearchiter=linesearchiter)
  
     prob = obj.get_problem(atol=atol, precond=UseComplexSpaceOptimizer)
  
@@ -52,7 +52,7 @@ def solve(Nmesh, x0, rtol, run, Nsm, prefix, mock_model, obj, data_p, truth_pm, 
             fit_p = mock_model.make_observable(state['x'])
             if state.nit % saveit == 0:
                 fit_p.save(optfolder + '%s/%04d/fit_p' % (run, state['nit']))
-            r = dg.evaluate(fit_p, data)
+            r = dg.evaluate(fit_p, data_p)
             #obj.save_report(r, optfolder + "%s/%s_N%02d-%04d.png"% (run, prefix, int(Nsm*10), state['nit']))
             dg.save_report(r, optfolder + "%s/%s_N%02d-%03d.png"% (run, prefix, int(Nsm*10), state['nit']), pm)
             dg.save_2ptreport(r, optfolder + "%s/2pt/%s_N%02d-%03d.png"% (run, prefix, int(Nsm*10), state['nit']), pm)
@@ -64,7 +64,7 @@ def solve(Nmesh, x0, rtol, run, Nsm, prefix, mock_model, obj, data_p, truth_pm, 
     state = optimizer.minimize(prob, x0=x0, monitor=monitor)
     fit_p = mock_model.make_observable(state['x'])
     fit_p.save(optfolder + '%s/best-fit' % run)
-    r = dg.evaluate(fit_p, data)
+    r = dg.evaluate(fit_p, data_p)
     dg.save_report(r, optfolder + "%s/best-fit.png" % run, pm)
     dg.save_2ptreport(r, optfolder + "%s/2pt/%s_N%02d-best-fit.png" % (run, prefix, int(Nsm*10)), pm)
     return state.x
